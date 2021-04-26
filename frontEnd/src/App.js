1f5211792ffbcaad6493d14a1b6a8e078f49a080
reni11111 <renaldo@tegeria.com>
2021-04-26 02:46:00 +0200

    hosting + demo link

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,25 +1,20 @@
 import React from'react'
 import Amplify from'aws-amplify'
-import{ Authenticator, } from'aws-amplify-react'
+import{ Authenticator, withAuthenticator } from'aws-amplify-react'
 import Todos from'./Todos'
 import cognitoConfig from'./config/cognito.json'
 
 Amplify.configure(cognitoConfig)
 
 function App() {
-
   return(
-    <>
-      <Authenticator
-        usernameAttributes="email"
-        signUpConfig={{
-          hiddenDefaults: ['phone_number'],
-        }}
-      >
-        <Todos />
-      </Authenticator>
-    </>
+    <Todos />
   )
 }
 
-export default App
+export default withAuthenticator(App, {
+  usernameAttributes: 'email',
+  signUpConfig: {
+    hiddenDefaults: ['phone_number'],
+  }
+})
